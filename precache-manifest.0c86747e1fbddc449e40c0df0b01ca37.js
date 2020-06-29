7edd50bd2f5865bb6ff7033a1e23929671e63762
iamtigermaximus <46716968+iamtigermaximus@users.noreply.github.com>
2020-06-29 17:38:02 +0300

    Updates

---
+++ precache-manifest.0c86747e1fbddc449e40c0df0b01ca37.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e4238ea6db2f7d681aae5a555704e78",
+    "url": "/My-Portfolio-Website/index.html"
+  },
+  {
+    "revision": "4a9ac2ba230e253fc0e9",
+    "url": "/My-Portfolio-Website/static/css/main.e5035580.chunk.css"
+  },
+  {
+    "revision": "cc151bc942b058e9f730",
+    "url": "/My-Portfolio-Website/static/js/2.a2b910fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/My-Portfolio-Website/static/js/2.a2b910fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a9ac2ba230e253fc0e9",
+    "url": "/My-Portfolio-Website/static/js/main.44bcbbd0.chunk.js"
+  },
+  {
+    "revision": "b6003dc6ea7322aa4218",
+    "url": "/My-Portfolio-Website/static/js/runtime-main.de8b9a7b.js"
+  },
+  {
+    "revision": "5a78036a4c5188f47d02f95fba191c36",
+    "url": "/My-Portfolio-Website/static/media/BudgetApp.5a78036a.jpg"
+  },
+  {
+    "revision": "703849b645d62aa577fb3d2a06838ffa",
+    "url": "/My-Portfolio-Website/static/media/CovidTrackerApp.703849b6.jpg"
+  },
+  {
+    "revision": "a1138b6e26e4d4b037d3108fa0531230",
+    "url": "/My-Portfolio-Website/static/media/PortfolioParallax.a1138b6e.jpg"
+  },
+  {
+    "revision": "dc558c015e24f9d718ec2a938d61fee2",
+    "url": "/My-Portfolio-Website/static/media/ResponsiveWebsite.dc558c01.jpg"
+  },
+  {
+    "revision": "e353315225a9fa88abe255caaf506df8",
+    "url": "/My-Portfolio-Website/static/media/ToDoApp.e3533152.jpg"
+  },
+  {
+    "revision": "959f22f11273d0c28dc2ef39167bd981",
+    "url": "/My-Portfolio-Website/static/media/starry.959f22f1.jpg"
+  },
+  {
+    "revision": "2b0e776617aa0afd56af662633ddbe23",
+    "url": "/My-Portfolio-Website/static/media/techfolio.2b0e7766.png"
+  }
+]);
